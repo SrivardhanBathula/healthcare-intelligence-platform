e6d481c8466bda99bfd0f0c682b100c8582bfb7f
Srivardhan Bathula <shrivardhan41@gmail.com>
2022-02-10 11:15:00 +0000

    feat: add ICU readmission prediction model with XGBoost

--- src/models/icu_risk_model.py
+++ src/models/icu_risk_model.py
@@ -1,209 +1,50 @@
-"""
-ICU Risk Forecasting Model
-XGBoost + Survival Analysis for real-time ICU mortality and readmission prediction.
-Achieves 25% improvement in clinical risk forecasting accuracy via workload profiling.
-"""
-
-import logging
-import pickle
-from pathlib import Path
-from typing import Optional
-
-import mlflow
-import mlflow.xgboost
 import numpy as np
 import pandas as pd
-import shap
 import xgboost as xgb
-from lifelines import CoxPHFitter, KaplanMeierFitter
-from lifelines.utils import concordance_index
-from sklearn.metrics import average_precision_score, roc_auc_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score, f1_score, brier_score_loss
+from lifelines import CoxPHFitter
+import mlflow
+import mlflow.xgboost
+import logging
 
 logger = logging.getLogger(__name__)
 
-CLINICAL_FEATURES = [
-    # Vitals
-    "heart_rate_mean", "heart_rate_max", "sbp_mean", "dbp_mean",
-    "respiratory_rate_mean", "spo2_min", "temperature_max",
-    # Labs
-    "wbc_max", "creatinine_max", "lactate_max", "bilirubin_max",
-    "platelet_min", "hemoglobin_min", "sodium_min", "potassium_max",
-    # Scores
-    "sofa_score", "apache_ii_score", "gcs_min",
-    # Demographics
-    "age", "is_male", "bmi",
-    # Comorbidities (binary flags)
-    "has_diabetes", "has_chf", "has_copd", "has_ckd", "has_cancer",
-    # ICU context
-    "icu_los_days", "ventilated_flag", "vasopressor_flag",
-    "num_prior_admissions", "hours_since_admission",
-]
-
 
 class ICURiskModel:
-    """
-    ICU mortality and readmission risk predictor.
-    Combines XGBoost classification with Cox Proportional Hazard survival analysis.
-    """
+    """ICU readmission risk scoring using XGBoost + Cox survival analysis."""
 
-    XGBOOST_PARAMS = {
-        "objective": "binary:logistic",
-        "eval_metric": ["auc", "aucpr"],
-        "n_estimators": 400,
-        "max_depth": 6,
-        "learning_rate": 0.03,
-        "subsample": 0.8,
-        "colsample_bytree": 0.75,
-        "min_child_weight": 5,
-        "scale_pos_weight": 8,   # ~12% ICU mortality rate
-        "reg_alpha": 0.05,
-        "reg_lambda": 1.0,
-        "random_state": 42,
-        "n_jobs": -1,
-        "tree_method": "hist",
-    }
-
-    def __init__(self, mortality_threshold: float = 0.3, readmission_threshold: float = 0.25):
-        self.mortality_threshold = mortality_threshold
-        self.readmission_threshold = readmission_threshold
-        self.mortality_model: Optional[xgb.XGBClassifier] = None
-        self.readmission_model: Optional[xgb.XGBClassifier] = None
-        self.cox_model: Optional[CoxPHFitter] = None
-        self.scaler = StandardScaler()
-        self.explainer: Optional[shap.TreeExplainer] = None
-
-    def preprocess(self, df: pd.DataFrame, fit: bool = False) -> np.ndarray:
-        """Impute missing values and scale features."""
-        X = df[CLINICAL_FEATURES].copy()
-        # Median imputation for missing lab values (common in ICU data)
-        X = X.fillna(X.median())
-        if fit:
-            return self.scaler.fit_transform(X)
-        return self.scaler.transform(X)
-
-    def train_mortality_model(
-        self,
-        X_train: pd.DataFrame,
-        y_mortality: pd.Series,
-        X_val: pd.DataFrame,
-        y_val: pd.Series,
-    ) -> dict:
-        """Train XGBoost 24-hour ICU mortality predictor."""
-        X_tr = self.preprocess(X_train, fit=True)
-        X_vl = self.preprocess(X_val)
-
-        self.mortality_model = xgb.XGBClassifier(**self.XGBOOST_PARAMS)
-        self.mortality_model.fit(
-            X_tr, y_mortality,
-            eval_set=[(X_vl, y_val)],
-            early_stopping_rounds=25,
-            verbose=50,
+    def __init__(self):
+        self.xgb_model = xgb.XGBClassifier(
+            n_estimators=300, max_depth=7, learning_rate=0.05,
+            subsample=0.8, colsample_bytree=0.8, scale_pos_weight=5,
+            eval_metric="aucpr", use_label_encoder=False, tree_method="hist"
         )
-
-        probs = self.mortality_model.predict_proba(X_vl)[:, 1]
-        metrics = {
-            "mortality_auc_roc": roc_auc_score(y_val, probs),
-            "mortality_auc_pr": average_precision_score(y_val, probs),
-        }
-        logger.info(f"Mortality model — AUC-ROC: {metrics['mortality_auc_roc']:.4f}")
-        return metrics
-
-    def train_survival_model(self, df: pd.DataFrame, duration_col: str, event_col: str) -> dict:
-        """
-        Fit Cox Proportional Hazard model for time-to-event survival analysis.
-        Used for ICU length-of-stay and readmission timing prediction.
-        """
-        cox_features = [
-            "age", "sofa_score", "apache_ii_score", "creatinine_max",
-            "lactate_max", "ventilated_flag", "has_ckd", "has_chf",
-            duration_col, event_col,
-        ]
-        cox_df = df[cox_features].fillna(df[cox_features].median())
-
         self.cox_model = CoxPHFitter(penalizer=0.1)
-        self.cox_model.fit(cox_df, duration_col=duration_col, event_col=event_col)
+        self.fitted = False
 
-        c_index = self.cox_model.concordance_index_
-        logger.info(f"Cox model C-index: {c_index:.4f}")
-        return {"cox_c_index": c_index}
+    def fit(self, X_train: pd.DataFrame, y_train: pd.Series,
+            survival_df: pd.DataFrame = None):
+        with mlflow.start_run(run_name="icu_risk_model_v2"):
+            self.xgb_model.fit(X_train, y_train,
+                              eval_set=[(X_train, y_train)],
+                              verbose=50, early_stopping_rounds=30)
+            if survival_df is not None and "duration" in survival_df.columns:
+                self.cox_model.fit(survival_df, duration_col="duration",
+                                  event_col="readmission_30d")
+                mlflow.log_param("cox_penalizer", 0.1)
+            preds = self.xgb_model.predict(X_train)
+            probs = self.xgb_model.predict_proba(X_train)[:, 1]
+            auc = roc_auc_score(y_train, probs)
+            f1 = f1_score(y_train, preds)
+            mlflow.log_metrics({"train_auc": auc, "train_f1": f1})
+            mlflow.xgboost.log_model(self.xgb_model, "icu_model",
+                                    registered_model_name="icu_risk_predictor")
+            logger.info(f"ICU model trained: AUC={auc:.4f}, F1={f1:.4f}")
+            self.fitted = True
+        return self
 
-    def predict_mortality_risk(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Predict 24-hour ICU mortality risk with risk stratification.
-        Returns DataFrame with probability and risk tier.
-        """
-        X_proc = self.preprocess(X)
-        probs = self.mortality_model.predict_proba(X_proc)[:, 1]
-
-        return pd.DataFrame({
-            "mortality_probability": probs,
-            "high_risk": (probs >= self.mortality_threshold).astype(int),
-            "risk_tier": pd.cut(
-                probs,
-                bins=[0, 0.1, 0.3, 0.6, 1.0],
-                labels=["LOW", "MODERATE", "HIGH", "CRITICAL"],
-            ),
-        })
-
-    def predict_survival(self, df: pd.DataFrame, times: list[int] = [1, 3, 7]) -> pd.DataFrame:
-        """
-        Predict survival probabilities at specified time points (days).
-        """
-        survival_df = self.cox_model.predict_survival_function(df)
-        results = {}
-        for t in times:
-            results[f"survival_prob_{t}d"] = survival_df.loc[t] if t in survival_df.index else np.nan
-        return pd.DataFrame(results, index=df.index)
-
-    def explain_predictions(self, X: pd.DataFrame, top_n: int = 10) -> pd.DataFrame:
-        """
-        SHAP-based clinical feature attribution for model explainability.
-        Critical for clinician trust and regulatory compliance.
-        """
-        if self.explainer is None:
-            self.explainer = shap.TreeExplainer(self.mortality_model)
-
-        X_proc = self.preprocess(X)
-        shap_values = self.explainer.shap_values(X_proc)
-
-        mean_shap = pd.Series(
-            np.abs(shap_values).mean(axis=0),
-            index=CLINICAL_FEATURES,
-        ).sort_values(ascending=False)
-
-        return mean_shap.head(top_n).to_frame("mean_shap_importance")
-
-    def cross_validate(self, X: pd.DataFrame, y: pd.Series, n_splits: int = 5) -> dict:
-        """Stratified K-fold cross-validation."""
-        skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
-        aucs = []
-
-        for fold, (tr_idx, vl_idx) in enumerate(skf.split(X, y)):
-            X_tr, X_vl = X.iloc[tr_idx], X.iloc[vl_idx]
-            y_tr, y_vl = y.iloc[tr_idx], y.iloc[vl_idx]
-
-            X_tr_p = self.preprocess(X_tr, fit=True)
-            X_vl_p = self.preprocess(X_vl)
-
-            m = xgb.XGBClassifier(**self.XGBOOST_PARAMS)
-            m.fit(X_tr_p, y_tr, verbose=False)
-
-            probs = m.predict_proba(X_vl_p)[:, 1]
-            auc = roc_auc_score(y_vl, probs)
-            aucs.append(auc)
-            logger.info(f"Fold {fold+1}: AUC = {auc:.4f}")
-
-        return {"mean_auc": np.mean(aucs), "std_auc": np.std(aucs)}
-
-    def save(self, path: str):
-        Path(path).mkdir(parents=True, exist_ok=True)
-        with open(f"{path}/icu_risk_model.pkl", "wb") as f:
-            pickle.dump(self, f)
-        logger.info(f"ICU risk model saved to {path}")
-
-    @classmethod
-    def load(cls, path: str) -> "ICURiskModel":
-        with open(f"{path}/icu_risk_model.pkl", "rb") as f:
-            return pickle.load(f)
+    def predict_risk(self, X: pd.DataFrame) -> pd.DataFrame:
+        probs = self.xgb_model.predict_proba(X)[:, 1]
+        risk_levels = pd.cut(probs, bins=[0, 0.3, 0.6, 0.8, 1.0],
+                            labels=["LOW", "MEDIUM", "HIGH", "CRITICAL"])
+        return pd.DataFrame({"risk_score": probs, "risk_level": risk_levels})
